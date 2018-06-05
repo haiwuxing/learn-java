bd571efc950289f966cfdb3e8b58abd377b585f8
Li Jian <lijian@hantek.com>
2018-06-05 09:18:15 +0800

    阅读理解完代码

--- SyncArray/SyncArrayDemo/src/SyncArrayDemo.java
+++ SyncArray/SyncArrayDemo/src/SyncArrayDemo.java
@@ -5,14 +5,15 @@ public class SyncArrayDemo {
 	 * 同步访问一个数组Integer[10]，生产者不断地往数组放入整数1000，数组满时等待；消费者不断地将数组里面的数置零，数组空时等待。
 	 */
       
-    private static final Integer[] val=new Integer[10];
+    private static final Integer[] array =new Integer[10];
     private static int size=0;
     private static final int MAX_SIZE=10;
       
     public static void main(String[] args)  {
     	SyncArrayDemo pc=new SyncArrayDemo();
-        new Thread(pc.new P()).start();
-        new Thread(pc.new C()).start();
+    	new Thread(pc.new Consumer()).start();
+        new Thread(pc.new Produce()).start();
+        
     }  
   
     public synchronized void consume(){
@@ -24,7 +25,7 @@ public class SyncArrayDemo {
                     e.printStackTrace();
                 }
             }  
-            val[--size]=0;
+            array[--size]=0;
             notifyAll();
             System.out.println("successful consume and now size="+size);
     }
@@ -38,20 +39,22 @@ public class SyncArrayDemo {
                     e.printStackTrace();
                 }  
             }  
-            val[size++]=value;
+            array[size++]=value;
             notifyAll();  
             System.out.println("successful produce and now size="+size);
     }  
       
-    class P implements Runnable{
+    class Produce implements Runnable{
         public void run(){
-            while(true)produce(1000);
+            while(true)
+            	produce(1000);
         }
     }
     
-    class C implements Runnable{
+    class Consumer implements Runnable{
         public void run(){
-            while(true)consume();
+            while(true)
+            	consume();
         }
     }
 
