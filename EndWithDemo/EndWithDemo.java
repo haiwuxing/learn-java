950dac6a3102b48d37080c8cbfd1f9acfc503f56
Li Jian <lijian@hantek.com>
2018-06-09 14:35:25 +0800

    Add EndWithDemo project

---
+++ EndWithDemo/EndWithDemo.java
@@ -0,0 +1,12 @@
+public class EndWithDemo {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+
+        String str = "abc.CSV";
+        if (str.endsWith(".csv")) {
+            System.out.println("以 .csv 结尾");
+        } else {
+            System.out.println("不以 .csv 结尾");
+        }
+    }
+}
---
+++ EndWithDemo/README.md
@@ -0,0 +1,3 @@
+String.endsWith 区分大小写吗？
+
+结论：区分
